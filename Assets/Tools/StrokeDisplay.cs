25193678f2613088b991910607c760e8ac4342e7
B9915049 <f59107@gmail.com>
2013-05-25 14:55:00 +0800

    Display Update

--- Assets/Tools/StrokeDisplay.cs
+++ Assets/Tools/StrokeDisplay.cs
@@ -3,43 +3,166 @@ using System.Collections;
 
 public class StrokeDisplay : MonoBehaviour {
 	
-	public LineRenderer lineRenderer;
+	public DisplayStyle style;
+	public DisplayAnimation animation;
+	public GameObject effect;
 	
+	public float stepLength;
+	public Vector3[] DrawPointList;
+	public int DPLSize;
 	private Vector3 offset;
-	private Stroke targetStroke;
-	private int tmpCount;
+	public Stroke targetStroke;
 	// Use this for initialization
 	void Start () {
-		lineRenderer.enabled = false;
-		offset = Vector3.back*0.1f;
+		offset = Vector3.back;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(targetStroke == null) return;
-		if(targetStroke.pointList.Count < 2) lineRenderer.enabled = false;
-		else lineRenderer.enabled = true;
 		
-		if(tmpCount != targetStroke.pointList.Count)
+		
+		if(animation != DisplayAnimation.Default)
+			DoAnimation();
+		DoStyle();
+	}
+	
+	private float styDelay = 0.0f;
+	private int targetIndex = 0;
+	private int lastIndex = 0;
+	public void DoStyle()
+	{
+		switch(style)
 		{
-			tmpCount = targetStroke.pointList.Count;
-			SetRenderer();
+		case DisplayStyle.Default:
+			if(styDelay < 0)
+			{
+				for(int i = 0; i < DPLSize; i++)
+				{
+					effect.transform.position = DrawPointList[i];
+					effect.particleSystem.Emit(1);
+				}
+				styDelay = effect.particleSystem.startLifetime/10.0f;
+			}else styDelay -= Time.deltaTime; 
+			break;
+			
+		case DisplayStyle.Flicker:
+			if(styDelay < 0)
+			{
+				for(int i = 0; i < DPLSize; i++)
+				{
+					effect.transform.position = DrawPointList[i];
+					effect.particleSystem.Emit(1);
+				}
+				styDelay = effect.particleSystem.startLifetime;
+			}else styDelay -= Time.deltaTime; 
+			break;
+			
+		case DisplayStyle.Loop:
+			targetIndex = (targetIndex+(int)(Time.deltaTime*100.0f))%DPLSize;
+			if(lastIndex > targetIndex) lastIndex = 0;
+			for(int i = lastIndex; i < targetIndex; i++)
+			{
+				effect.transform.position = DrawPointList[i];
+				effect.particleSystem.Emit(1);
+			}
+			lastIndex = targetIndex;
+			break;
+			
+		case DisplayStyle.RLoop:
+			targetIndex = (targetIndex-(int)(Time.deltaTime*100.0f))%DPLSize;
+			if(targetIndex<0) targetIndex += DPLSize;
+			if(lastIndex < targetIndex) lastIndex = DPLSize-1;
+			for(int i = lastIndex; i > targetIndex; i--)
+			{
+				effect.transform.position = DrawPointList[i];
+				effect.particleSystem.Emit(1);
+			}
+			lastIndex = targetIndex;
+			break;
+			
+		default:
+			style = DisplayStyle.Default;
+			break;
+		}
+	}
+	
+	private float aniDelay;
+	public void DoAnimation()
+	{
+		switch(animation)
+		{
+		case DisplayAnimation.Default:
+			break;
+			
+		case DisplayAnimation.Focus:
+			aniDelay += Time.deltaTime;
+			for(int i = 0; i < DPLSize; i++)
+			{
+				DrawPointList[i] = Vector3.MoveTowards(DrawPointList[i], DrawPointList[DPLSize/2], Time.deltaTime*10);
+			}
+			if(aniDelay >= 1.0f)
+			{
+				aniDelay = 0.0f;
+				animation = DisplayAnimation.Default;
+			}
+			break;
+			
+		default:
+			animation = DisplayAnimation.Default;
+			break;
 		}
 	}
 	
 	public void SetTarget(Stroke stroke)
 	{
 		targetStroke = stroke;
-		if(stroke.pointList.Count==0) return;
-		SetRenderer();
+		MakeDrawPoint(2.0f, stroke.pointList);
+		Debug.Log("SetTarget");
 	}
 	
-	void SetRenderer()
+	private void MakeDrawPoint(float stepLength, ArrayList pointList)
 	{
-		lineRenderer.SetVertexCount(tmpCount);
-		for(int i = 0; i < tmpCount; i++)
+		if(pointList.Count == 0)
 		{
-			lineRenderer.SetPosition(i, (Vector3)targetStroke.pointList[i]+offset);
+			DrawPointList = null;
+			return;
+		}
+		if(pointList.Count == 1)
+		{
+			DrawPointList = new Vector3[1];
+			DrawPointList[0] = (Vector3)pointList[0];
+			return;
+		}
+		Debug.Log("MakeDrawPoint");
+		float totalLength = 0.0f;
+		for(int i = 0; i < pointList.Count-1; i++)
+		{
+			totalLength += Vector3.Distance((Vector3)pointList[i], (Vector3)pointList[i+1]);
+		}
+		DPLSize = (int)(totalLength/stepLength);
+		DrawPointList = new Vector3[DPLSize];
+		Vector3 curPos = (Vector3)pointList[0];
+		Vector3 prePos = (Vector3)pointList[0];
+		int curPointIndex = 0;
+		float tmpLength = 0.0f;
+		float curLength = Vector3.Distance(DrawPointList[0], DrawPointList[1]);
+		float preLength = 0.0f;
+		
+		DrawPointList[0] = curPos;
+		for(int i = 1; i < DPLSize; i++)
+		{
+			tmpLength = stepLength*i;
+			while(curLength < tmpLength)
+			{
+				prePos = curPos;
+				preLength = curLength;
+				curPointIndex++;
+				curPos = (Vector3)pointList[curPointIndex];
+				curLength += Vector3.Distance(curPos, prePos);
+			}
+			
+			DrawPointList[i] = Vector3.Lerp(prePos, curPos, (curLength-tmpLength)/(curLength-preLength));
 		}
 	}
 }
--- Assets/Tools/WordDisplay.cs
+++ Assets/Tools/WordDisplay.cs
@@ -1,36 +1,61 @@
 using UnityEngine;
 using System.Collections;
 
+public enum DisplayStyle {
+	Default,
+	Loop,
+	RLoop,
+	Flicker
+}
+
+public enum DisplayAnimation {
+	Default, // do nothing
+	Focus
+}
+
+public enum DrawRank {
+	S = 0,
+	A = 1,
+	B = 2,
+	C = 3,
+	D = 4,
+	E = 5
+}
+
 public class WordDisplay : MonoBehaviour {
 	
-	public GameObject strokeDisplayer;
 	public Color color;
-	
-	private StrokeDisplay[] strokeDisplayList;
+	public StrokeDisplay[] strokeDisplayList;
+	public GameObject effect;
+
 	private Word targetWord;
+	
 	private int curSize = 0;
 	private int maxSize = 100;
+	private int tmpCount;
+	private Vector3 offset;
+	private Stroke DrawingStroke;
 	// Use this for initialization
 	void Start () {
+		offset = Vector3.back;
+		effect = (GameObject)Instantiate(effect);
 		strokeDisplayList = new StrokeDisplay[100];
 		for(int i = 0; i < maxSize; i++)
 		{
-			
-			GameObject GO = (GameObject)Instantiate(strokeDisplayer);
-			GO.transform.parent = this.transform;
-			strokeDisplayList[i] = GO.GetComponent<StrokeDisplay>();
-			strokeDisplayList[i].lineRenderer.SetColors(color, color);
+			strokeDisplayList[i] = this.gameObject.AddComponent<StrokeDisplay>();	
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(targetWord == null) return;
+		if( targetWord == null ) return;
 		if( curSize > maxSize ) return;
 		
-		if( curSize < targetWord.strokeList.Count)
+		
+		if( curSize < targetWord.finishIndex)
 		{
 			strokeDisplayList[curSize].SetTarget((Stroke)targetWord.strokeList[curSize]);
+			strokeDisplayList[curSize].enabled = true;
 			curSize++;
 		}
 	}
@@ -49,10 +74,16 @@ public class WordDisplay : MonoBehaviour {
 		curSize = word.strokeList.Count;
 		for(int i = 0; i < maxSize; i++)
 		{
+			strokeDisplayList[i].effect = effect;
 			if( i < targetWord.strokeList.Count )
+			{
 				strokeDisplayList[i].SetTarget( (Stroke)targetWord.strokeList[i] );
+				strokeDisplayList[i].enabled = true;
+			}
 			else
-				strokeDisplayList[i].SetTarget( new Stroke() );
+			{
+				strokeDisplayList[i].enabled = false;
+			}
 		}
 	}
 }
